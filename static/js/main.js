238f157c2430ad2fdb1cdabf752675cfbea0944a
Patrick2052 <pa.huebler@gmail.com>
2022-11-11 16:26:43 +0100

    stuff

--- static/js/main.js
+++ static/js/main.js
@@ -1,19 +1,19 @@
-const heroSection = document.getElementById("hero-section")
+// const heroSection = document.getElementById("hero-section")
 
-const handleMouseMove = (e) => {
-    const {currentTarget: target} = e;
+// const handleMouseMove = (e) => {
+//     const {currentTarget: target} = e;
     
-    const rect = target.getBoundingClientRect();
+//     const rect = target.getBoundingClientRect();
     
-    const x = e.clientX - rect.left;
-    const y = e.clientY - rect.top;
+//     const x = e.clientX - rect.left;
+//     const y = e.clientY - rect.top;
 
-    target.style.setProperty("--mouse-x", `${x/100}px`)
-    target.style.setProperty("--mouse-y", `${y/100}px`)
+//     target.style.setProperty("--mouse-x", `${x/100}px`)
+//     target.style.setProperty("--mouse-y", `${y/100}px`)
 
-}
+// }
 
-heroSection.onmousemove = e => handleMouseMove(e)
+// heroSection.onmousemove = e => handleMouseMove(e)
 
 document.getElementById("hero-section").addEventListener("mouseenter", (e) => {
     console.log("entered")
